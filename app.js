63544a34abfff28b015614de2c07956327721237
WEB-20-1-1 <74668645+WEB-20-1-1@users.noreply.github.com>
2021-07-12 15:27:17 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+let name1 = prompt('Введите ваше имя: ');
+alert(`${name1}`);
+
+
+
+/* let name = prompt("Введите ваше ФИО");
+let name2 = prompt("Введите дату рождения");
+alert(`Добрый день !`); */
